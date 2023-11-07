5464ad8932af60be2a1fa9dbae2da339e1633859
ИСП-22 <-22@LAB30-09>
2023-11-07 09:04:46 +0300

    Добавьте файлы проекта.

---
+++ MultiplesOfThree/Program.cs
@@ -0,0 +1,25 @@
+﻿
+        int[] numbers = { 1, 4, 3, 9, 12, 7, 6, 15, 8 };
+        int sum = 0;
+
+        Console.WriteLine("Исходный массив чисел:");
+        PrintArray(numbers);
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 3 == 0)
+            {
+                sum += numbers[i];
+            }
+        }
+
+        Console.WriteLine("Сумма всех чисел, кратных 3: " + sum);
+
+    static void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
